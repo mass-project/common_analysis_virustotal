0ad8523b14d7fc7657de294a439cb3fa327645a7
Timm Behner <behner@cs.uni-bonn.de>
2016-04-14 08:56:19 +0200

    Common Analysis for Virustotal

--- common_analysis_virustotal/virustotal_analysis.py
+++ common_analysis_virustotal/virustotal_analysis.py
@@ -1,10 +1,11 @@
 from common_analysis_base import AnalysisPlugin, FileAnalysisMixin, URLAnalysisMixin, IPAnalysisMixin, DomainAnalysisMixin
-from common_helper_files import get_directory_for_filename, get_version_string_from_git, read_in_chunks, md5sum
-from virus_total_apis import PublicApi as VirustotalPublicApi
+from common_helper_files import get_directory_for_filename, get_version_string_from_git, md5sum
+from virus_total_apis import PrivateApi as VirustotalApi
 import logging
-import pprint
-import asyncio
 import time
+import datetime
+import random
+import threading
 
 logger = logging.getLogger('CommonAnalysisVirusTotal')
 logger.setLevel(logging.INFO)
@@ -16,43 +17,81 @@ VT_FILE_UNKOWN = 0
 VT_FILE_REPORT_SUBMITTED = 1
 
 
-class CommonAnalysisVirusTotal(AnalysisPlugin, FileAnalysisMixin, URLAnalysisMixin, IPAnalysisMixin, DomainAnalysisMixin ):
-    def __init__(self, api_key, sleep_time=16):
+class DelayedVirustotalApi():
+    last_request_time = datetime.datetime(1970, 1, 1)
+    lock = threading.Lock()
+
+    def __init__(self, api_key, backoff_time):
+        self.api = VirustotalApi(api_key)
+        self.backoff_time = backoff_time
+        random.seed()
+        rand_seconds = random.randrange(3, 16)
+        self.last_request_time = datetime.datetime.now() - datetime.timedelta(seconds=rand_seconds)
+
+    def __getattr__(self, name):
+        def delay_wrapper(*args, **kwargs):
+            with self.lock:
+                now = datetime.datetime.now()
+                time_delta = now - self.last_request_time
+                if time_delta.seconds >= self.backoff_time:
+                    logger.info('VT_DELAY calling {} (time_delta={})'.format(name, time_delta.seconds))
+                else:
+                    cur_backoff_time = self.backoff_time - time_delta.seconds
+                    logger.info('Called to soon, wait for {}sec'.format(cur_backoff_time))
+                    time.sleep(cur_backoff_time + 1)
+                    logger.info('VT_DELAY calling {}'.format(name))
+                method = getattr(self.api, name)
+                vt_ret_value = method(*args, **kwargs)
+                self.last_request_time = datetime.datetime.now()
+            return vt_ret_value
+        return delay_wrapper
+
+
+class CommonAnalysisVirusTotal(AnalysisPlugin, FileAnalysisMixin, URLAnalysisMixin, IPAnalysisMixin, DomainAnalysisMixin):
+    def __init__(self, api_key, backoff_time=16):
         """
         CommonAnalysisVirusTotal
 
-        :param api_key: API Key for Virustotal 
-        :param sleep_time: Time to sleep between requests to Virustotal in seconds.
+        :param api_key: API Key for Virustotal
+        :param backoff_time: Time to sleep between requests to Virustotal in seconds.
         """
         super(CommonAnalysisVirusTotal, self).__init__(system_version)
-        self.virustotal = VirustotalPublicApi(api_key)
+        self.virustotal = DelayedVirustotalApi(api_key, backoff_time)
 
-    def _send_file_to_virustotal(self,file_path):
-        self.virustotal.scan_file(file_path)
+    def _send_file_to_virustotal(self, file_path):
+        resp = self.virustotal.scan_file(file_path)
+        if 'status_code' in resp and resp['status_code'] != 201:
+            raise RuntimeError
         logger.info('{} uploaded to Virustotal.'.format(file_path))
 
-    def _get_virustotal_file_result(self,file_path):
+    def _get_virustotal_file_result(self, file_path):
         with open(file_path, 'rb') as f:
             file_md5 = md5sum(f)
-        return self.virustotal.get_file_report(file_md5)['results']
+        vt_response = self.virustotal.get_file_report(file_md5)
+        return vt_response['results']
 
     def wait_for_report(self, resource):
         response_code = VT_FILE_ANALYZIS_PENDING
         report = self.prepare_analysis_report_dictionary()
+        if response_code == VT_FILE_UNKOWN:
+            raise RuntimeError("File is unkown to Virustotal.")
         while response_code != VT_FILE_REPORT_SUBMITTED:
-            time.sleep(self.sleep_time)
             logger.info('Requesting VirusTotal for result on {}'.format(resource))
-            result = self.virustotal.get_file_report(resource)['results']
+            vt_response = self.virustotal.get_file_report(resource)
+            if 'error' in vt_response:
+                logger.error('VirustotalApi Error: {}'.format(vt_response['error']))
+                continue
+            result = vt_response['results']
             response_code = result['response_code']
         report['result'] = result
         return report
 
-    def analyze_file(self, file_path, async=False):
+    def analyze_file(self, file_path, asynchronous=False):
         """
         Analyze a file.
 
         :param file_path: Path to the file.
-        :param async: If False the method waits until Virustotal has analyzed
+        :param asynchronous: If False the method waits until Virustotal has analyzed
         the file and returns the report. If True the Virustotal resource is
         returned as handle.
 
@@ -63,10 +102,10 @@ class CommonAnalysisVirusTotal(AnalysisPlugin, FileAnalysisMixin, URLAnalysisMix
         if result['response_code'] != VT_FILE_REPORT_SUBMITTED:
             self._send_file_to_virustotal(file_path)
 
-        if async:
+        if asynchronous:
             return result['resource']
         else:
-            return wait_for_report(result['resource'])
+            return self.wait_for_report(result['resource'])
 
     def analyze_ip(self, ip):
         report = self.prepare_analysis_report_dictionary()
@@ -85,4 +124,3 @@ class CommonAnalysisVirusTotal(AnalysisPlugin, FileAnalysisMixin, URLAnalysisMix
         result = self.virustotal.get_domain_report(domain)['results']
         report.update(result)
         return report
-
